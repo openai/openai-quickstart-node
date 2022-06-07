8082102492e93856f4c62f05070506ba0fe34162
Rajeev Nayak <jeev@openai.com>
2022-06-07 14:39:44 -0700

    upgrade node library

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,7 +6,8 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const completion = await openai.createCompletion("text-davinci-002", {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
     prompt: generatePrompt(req.body.animal),
     temperature: 0.6,
   });
