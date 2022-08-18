d2a20640cc3560549619811842a7b942b9524db6
Martin Connor <mepc36@gmail.com>
2022-08-18 14:23:15 -0400

    Generate raps that end with a specific word.

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,23 +6,49 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
+  const { input } = req.body
+
   const completion = await openai.createCompletion({
     model: "text-davinci-002",
-    prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
+    prompt: generatePrompt(input),
+    temperature: 1,
+    n: 1,
+    presence_penalty: 2.0,
+    frequency_penalty: 2.0,
+    max_tokens: 1000
   });
+  console.log('completion.data.choices:', completion.data.choices)
+
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(input) {
+  const examples = [
+    { word: `gold`, rap: `Tanqueray introduced me to her first cousin [LAST]Gold.` },
+    { word: `judgment`, rap: `no material possessions shall cloud my [LAST]judgement.` },
+    { word: `man`, rap: `I'm not a business, man, I'm a business [LAST]man.` },
+    { word: `rhyme`, rap: `The truth don't always rhyme.` },
+    { word: `dots`, rap: `Your last words will be damn what's all these red [LAST]dots.` },
+    { word: `cheesecake`, rap: `Nicki's on a diet but her pockets eating [LAST]cheesecake.` },
+    { word: `face`, rap: `Was it my whip appeal, or my baby face?` },
+    { word: `croissants`, rap: `Hurry up with my damn [LAST]croissants!` },
+  ]
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+  const formattedExamples = examples.map(example => {
+    const { word, rap } = example
+    const formattedExample = `
+    Word: ${word}
+    Rap: ${rap}
+    <--->`
+
+    return formattedExample
+  })
+
+  const prompt = `Give me a rap that ends with the word ${input}.
+${formattedExamples.join('')}
+  Word: ${input}
+  Rap:`
+  console.log('prompt:', prompt)
+
+  return prompt
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [input, setInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -13,11 +13,11 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({ input }),
     });
     const data = await response.json();
     setResult(data.result);
-    setAnimalInput("");
+    // setInput("");
   }
 
   return (
@@ -29,16 +29,16 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>GPT-3 Interface</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            placeholder="Enter an input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate GPT-3 output" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
