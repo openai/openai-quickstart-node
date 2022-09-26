ee2c6ed7cb747e8c030ce374ac09f424d9260381
Vaibhav <vaibhavgupta.spa@gmail.com>
2022-09-26 09:04:51 +0530

    Initial upload

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,24 +5,71 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function (req, res) {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
-  });
-  res.status(200).json({ result: completion.data.choices[0].text });
+let pim = {
+  name: "Vaibhav Gupta",
+  address: {
+    home: "89 Aggarsain Nagar, Near SS Jain Sabha, Sri Ganganagar, Rajasthan",
+    office: "3A4250 Microsoft main campus, Building 3, ISB Road, Gachibowli, Hyderabad",
+  },
+  pan: "ASDFG76543S",
+  aadhar: "098765431234",
+  contact: {
+    email: {
+      personal: "vaibhavgupta.spa@gmail.com",
+      work: "vaibgupta@microsoft.com"
+    },
+    phone: {
+      primary: "+91-8076173969",
+      secondary: "+91-8512095255",
+    },
+  },
+};
+
+let trainingData = `
+Compose a formal mail sharing the information as requested
+
+Prompt: Send items to my delivery address
+Response: Hi, 
+Thanks for reaching out. Kindly deliver the said items to my address – ${pim.address.home}. 
+Thanks and regards,
+${pim.name}
+
+Prompt: Share my work location
+Response: Dear Sir/ Ma’am, 
+Wish you’re doing well 
+You can find me at my work location which is ${pim.address.office}. Please let me know if you need further assistance with the directions. 
+Regards, 
+${pim.name}
+
+Prompt: Send my home address
+Response: Hi,
+Thanks for reaching out. Please find my current address mentioned below – 
+${pim.address.home}. 
+Do let me know if you need any other info. 
+With regards, 
+${pim.name}
+
+Prompt: Invite to my house party
+Response: Hey,
+Hope you're doing great! I am throwing a party at my place and you're invited. See you at ${pim.address.home} around 6pm tomorrow? Please RSVP. 
+Cheers, 
+${pim.name}
+`
+
+function generatePrompt(prompt) {
+  const capitalizedPrompt =
+    prompt[0].toUpperCase() + prompt.slice(1).toLowerCase();
+  return `${trainingData}+" "+
+Prompt: ${capitalizedPrompt}
+Message:`;
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+export default async function (req, res, temp) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    max_tokens: 2000,
+    prompt: generatePrompt(req.body.prompt),
+    temperature: temp/10,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
 }
