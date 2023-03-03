914cee044928bf3f5e461452739ecb69d77ce85e
Harry-Yates <hyates1@gmail.com>
2023-03-03 09:16:44 +0100

    added a new promt for gifts

---
+++ pages/api/generate-gifts.js
@@ -0,0 +1,55 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md"
+      }
+    });
+    return;
+  }
+
+  const animal = req.body.animal || "";
+  if (animal.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid animal"
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(animal),
+      temperature: 0.6
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch (error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: "An error occurred during your request."
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(priceMin, priceMax, gender, age, hobbies) {
+  return `suggest 3 Christmas gift ideas between ${priceMin} 
+  and ${priceMax}for a ${age} year old ${gender} 
+  that is into ${hobbies}.`;
+}
