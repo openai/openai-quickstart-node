1320ce4b7cbe5194aa32880b3b462bd7f448f07d
Harry-Yates <hyates1@gmail.com>
2023-03-03 09:34:06 +0100

    tidied up generate-gifts

--- pages/api/generate-gifts.js
+++ pages/api/generate-gifts.js
@@ -6,50 +6,21 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  if (!configuration.apiKey) {
-    res.status(500).json({
-      error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md"
-      }
-    });
-    return;
-  }
+  const { priceMin, priceMax, gender, age, hobbies } = req.body;
+  const prompt = generatePrompt(priceMin, priceMax, gender, age, hobbies);
 
-  const animal = req.body.animal || "";
-  if (animal.trim().length === 0) {
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid animal"
-      }
-    });
-    return;
-  }
+  console.log(prompt);
 
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6
-    });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch (error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: "An error occurred during your request."
-        }
-      });
-    }
-  }
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    temperature: 0.6
+  });
+
+  console.log(completion.data.choices[0].text);
+  res.status(200).json({ result: completion.data.choices[0].text });
 }
 
 function generatePrompt(priceMin, priceMax, gender, age, hobbies) {
-  return `suggest 3 Christmas gift ideas between ${priceMin} 
-  and ${priceMax}for a ${age} year old ${gender} 
-  that is into ${hobbies}.`;
+  return `suggest 3 Christmas gift ideas between ${priceMin} and ${priceMax}for a ${age} year old ${gender} that is into ${hobbies}.`;
 }
