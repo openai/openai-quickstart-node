6cda4a789e8c88f90cc014802e23105d5fe5e53e
jaapstronks <jaapstronks@bureaubolster.nl>
2023-03-04 17:31:50 +0100

    initial ocmmit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
 
+
 export default function Home() {
   const [topicInput, setTopicInput] = useState("");
   const [result, setResult] = useState();
@@ -31,27 +32,35 @@ export default function Home() {
     }
   }
 
+
+
+
   return (
     <div>
       <Head>
-        <title>Nieuwe Instituut bot</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Nieuwe Instituut Collection bot</title>
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main className={styles.main}>
         <img src="/ni-logo-small.png" className={styles.icon} />
-        <h3>Give feedback on my conference/exhibition idea</h3>
+        <img src="/maquette.jpeg" className={styles.image} alt="maquette Van Doesburg" />
+        <p className={styles.caption}>This is a model by architect and artist Theo van Doesburg.  Photo: Johannes Schwartz.</p>
+        <h3>What do you want to know?</h3>
+        <p className={styles.question}>Feel free to ask about more about this object. For example, you could ask: what is this a model of? How was it made? What is special about it? Which movement is this an icon of? Where can it be seen in person? Ask away! (<strong>Tip 1</strong>: each answer takes a couple of seconds to generate, so be patient after clicking the 'Get answer' button. <strong>Tip 2</strong>: if you don't know where to start, just ask 'What's so special about this?') </p>
+  
+        <div className={styles.result}>{result}</div>
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="topic"
-            placeholder="Enter a topic"
+            placeholder="What is so special about this?"
             value={topicInput}
             onChange={(e) => setTopicInput(e.target.value)}
           />
-          <input type="submit" value="Get feedback" />
+          <input type="submit" value="Get an answer" />
         </form>
-        <div className={styles.result}>{result}</div>
+        
       </main>
     </div>
   );
