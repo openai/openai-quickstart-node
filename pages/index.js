48724325a16838036367d99fac80859b2c743bd0
jaapstronks <jaapstronks@bureaubolster.nl>
2023-03-05 16:31:38 +0100

    fixes

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,17 @@ export default function Home() {
   const [result, setResult] = useState();
   const [isELI5Checked, setIsELI5Checked] = useState(false);
 
+  const questions = [
+    "What is interesting about this model??",
+    "How does this relate to 'The Style'?",
+    "What is the history behind this design?",
+    "How was this model made?",
+    "Who did Van Doesburg collaborate with?"
+  ];
+  
+  const randomIndex = Math.floor(Math.random() * 5);
+  const randomQuestion = questions[randomIndex];
+
   async function onSubmit(event) {
     event.preventDefault();
     try {
@@ -57,29 +68,33 @@ export default function Home() {
         <p className={styles.intro}>This is a prototype application, testing the usage of the <a href="https://openai.com/blog/introducing-chatgpt-and-whisper-apis">new ChatGPT API by OpenAI</a> to disclose collection information using the language model's general knowledge base combined with a small amount of custom training data and instructions. It can be used to convey information in a conversational interface, more tailored to the user's interests. Additionally, it is conceivable that the model could be user-configurable, switching to a different language or difficulty level. The prototype is created by Jaap Stronks.</p>
         <img src="/maquette.jpeg" className={styles.image} alt="maquette Van Doesburg" />
         <p className={styles.caption}>This is a model by architect and artist Theo van Doesburg.  Photo: Johannes Schwartz. </p>
-        <h3>What do you want to know?</h3>
-        <p className={styles.question}>Feel free to ask about more about this object. For example, you could ask: what is this a model of? How was it made? What is special about it? Which movement is this an icon of? Where can it be seen in person? Ask away! (<strong>Tip 1</strong>: each answer takes a couple of seconds to generate, so be patient after clicking the 'Get answer' button. <strong>Tip 2</strong>: if you don't know where to start, just ask 'What's so special about this?') </p>
+        <h3>What do you want to know about this object?</h3>
+        <p className={styles.question}>Feel free to ask about more about the interior model for l'Aubette. For example, you could ask: what is this a model of? How was it made? What is special about it? Which movement is this an icon of? Where can it be seen in person? Ask away!</p>
+        <div className={styles.listwrapper}>
+        <ul className={styles.list}><li><strong>Tip 1</strong>: each answer takes a couple of seconds to generate, so be patient after clicking the 'Get answer' button.</li><li> <strong>Tip 2</strong>: if you don't know where to start, just ask 'What's so special about this?'</li> </ul>
+        </div>
   
         <div className={styles.result}>{result}</div>
         <form onSubmit={onSubmit} className={styles.form}>
         <label className={styles.label}>
-        Ask a question
+        Ask a question about this object
         <input className={styles.input}
           type="text"
           name="topic"
-          placeholder="What is so special about this?"
+          placeholder={randomQuestion}
           value={topicInput}
           onChange={(e) => setTopicInput(e.target.value)}
         />
       </label>
       <label htmlFor="eli5">
         <input
+        className={styles.checkmark}
   type="checkbox"
   id="eli5"
   name="eli5"
   checked={isELI5Checked}
   onChange={() => setIsELI5Checked(!isELI5Checked)}
-/>Explain it like I'm five</label>
+/><span className={styles.labeltext}>Explain it like I'm five</span></label>
    
       <input type="submit" value="Get an answer" />
         </form>
