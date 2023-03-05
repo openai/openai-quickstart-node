8a71efb23df9bb85bd3550867502afe37b348c13
Destross <felipesagredogalindo@gmail.com>
2023-03-04 21:52:54 -0300

    Recodificando

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
