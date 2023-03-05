9f80af1757f81de8569c8867d9b7cc80d235fc62
jaapstronks <jaapstronks@bureaubolster.nl>
2023-03-05 14:07:57 +0100

    updates

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -16,6 +16,8 @@ export default async function (req, res) {
   }
 
   const topic = req.body.topic || '';
+  const isELI5 = req.body.isELI5 || false; // Add this line to get the value of the isELI5 checkbox
+
   if (topic.trim().length === 0) {
     res.status(400).json({
       error: {
@@ -28,8 +30,8 @@ export default async function (req, res) {
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(topic),
-      max_tokens: 300,
+      prompt: generatePrompt(topic, isELI5), // Pass the isELI5 value to the generatePrompt function
+      max_tokens: 800,
       temperature: 0.9,
 
     });
@@ -50,14 +52,24 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(topic) {
+
+function generatePrompt(topic, isELI5) {
+  console.log(isELI5);
   const capitalizedTopic =
     topic[0].toUpperCase() + topic.slice(1).toLowerCase();
-  return `I want you to answer a question about an interior model made by Theo van Doesburg. In your answer, use whatever you know about this topic, but also take into account the following information. Keep your answer under 500 characters, so be concise. Also, don't try to summarize or say too many things in every answer.  The person asking the questions will ask followup questions, so tell one specific tidbit of information at a time. Also, make 2 suggestions for related followup questions the person can ask you, if they want to to dive deeper into a certain topic. Suggest these by saying "You could also ask me about... or ..." and of course swap the '...' with the subjects (who should also be related to this interior model, of course) the person could ask about. One of these should be about the answer you just gave, offering to explain it further, and the other suggestion should be about one specific tidbit of information that was not mentioned in the current answer.
+  const prompt = `I want you to answer a question about an interior model made by Theo van Doesburg. In your answer, use whatever you know about this topic, but also take into account the following information. Also, don't try say too many things in every answer. The person asking the questions will ask followup questions, so tell one tidbit of information at a time, and expand a little about it. You can use the information provided in this prompt for that, but feel free to add existing knowledge to it. If you mention a certain style, place or person, use your general knowledge to explain what, where or who that is. Also, on a new line, make 1 suggestion for related followup questions the person can ask you, if they want to to dive deeper into a certain topic. Suggest these by saying "You could also ask me about..." or "Perhaps you would like to ask something about..." 
   The interior model that is the subject of the question is a colour design for a ceiling and three walls, for a cinema and dance hall of Café de l'Aubette in Strasbourg, France. It is a dazzlingly incongruous expression of the 1920s De Stijl movement. Designed by Theo van Doesburg, one of the movement’s founders and leading lights, the Aubette’s minimalist, geometric aesthetic was heavily influenced by the work of contemporary artists such as Piet Mondrian. In designing the café’s interiors, Van Doesburg sought to do more than simply place viewers before a painting; he wanted to envelop them in it. The design, from 1926, is for Café Aubette, one of De Stijl's most important architectural projects. As the pioneer of Dutch De Stijl movement, Theo van Doesburg employed his perspective on elementarism and the neo-plastic style to decorate the ceiling and wall of cinema-ballroom with orthogonal composition in primary colors. 
   It was made with pencil and gouache on cardboard, and it is 43 by 74.5 centimeters. It is the only model by Van Doesburg to survive after his death in 1931. At first sight, it seems to be a one-dimensional work. But the walls open outwards, as can be clearly seen in a photo from Van Doesburg’s studio. The model shows how Van Doesburg approached space as the synthesis of painting and architecture. The work is shown as it was acquired: framed. For decades, it was owned by the Swiss Galerie Gmurzynska and it was previously exhibited there as an autonomous visual work, framed on the wall. But such a strictly disciplinary approach was exactly what Van Doesburg was against: for him, a visual work could be an architectural project, model and autonomous artwork all at the same time. It is on display at het Nieuwe Instituut in Rotterdam, The Netherlands. In 1926, three avant-garde artists Theo van Doesburg, Sophie Taeuber-Arp and Jean Arp (or Hans Arp) were commissioned by Paul and Adré Horn to redecorate and design the Café Aubette in Strasbourg. Three artists were equally responsible for different sections of the building. Theo van Doesburg was in charge of the two cafés and two dance halls, Sophie Taeuber for the entrance aisle, tearoom, and two bars, and Jean Arp for the basement, the passage, and billiard room. And all three artists worked together designing the stairwell. The work of the three artists had been called "the Sistine Chapel of abstract art". This historical building still opens as a historical landmark nowadays.
- 
 
 Question: ${capitalizedTopic}
 Answer:`;
+
+  if (isELI5) {
+    console.log('eli5 was checked');
+    return `${prompt} Explain it to me like I'm a five year old.`;
+    
+  }
+
+  return prompt;
+  
 }
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,11 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 
+
 export default function Home() {
   const [topicInput, setTopicInput] = useState("");
   const [result, setResult] = useState();
+  const [isELI5Checked, setIsELI5Checked] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -15,7 +17,8 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ topic: topicInput }),
+        body: JSON.stringify({ topic: topicInput, isELI5: isELI5Checked}),
+
       });
 
       const data = await response.json();
@@ -39,28 +42,48 @@ export default function Home() {
     <div>
       <Head>
         <title>Nieuwe Instituut Collection bot</title>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="favicon" href="/favicon.png" />
       </Head>
 
-      <main className={styles.main}>
+     
+        <header className={styles.header}>
+        <div className={styles.headercontainer}>
         <img src="/ni-logo-small.png" className={styles.icon} />
+        </div>
+ 
+        </header>
+        <main className={styles.main}>
+        <h1 className={styles.title}>Nieuwe Instituut Collection bot</h1>
+        <p className={styles.intro}>This is a prototype application, testing the usage of the <a href="https://openai.com/blog/introducing-chatgpt-and-whisper-apis">new ChatGPT API by OpenAI</a> to disclose collection information using general a language model general knowledge base, added with a small amount of custom training data and instructions. It can be used to convey information in a conversational interface, more tailored to the user's interests. Additionally, it is conceivable that the model could be user-configurable, switching to a different language or difficulty level. The prototype is created by Jaap Stronks.</p>
         <img src="/maquette.jpeg" className={styles.image} alt="maquette Van Doesburg" />
-        <p className={styles.caption}>This is a model by architect and artist Theo van Doesburg.  Photo: Johannes Schwartz.</p>
+        <p className={styles.caption}>This is a model by architect and artist Theo van Doesburg.  Photo: Johannes Schwartz. </p>
         <h3>What do you want to know?</h3>
         <p className={styles.question}>Feel free to ask about more about this object. For example, you could ask: what is this a model of? How was it made? What is special about it? Which movement is this an icon of? Where can it be seen in person? Ask away! (<strong>Tip 1</strong>: each answer takes a couple of seconds to generate, so be patient after clicking the 'Get answer' button. <strong>Tip 2</strong>: if you don't know where to start, just ask 'What's so special about this?') </p>
   
         <div className={styles.result}>{result}</div>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="topic"
-            placeholder="What is so special about this?"
-            value={topicInput}
-            onChange={(e) => setTopicInput(e.target.value)}
-          />
-          <input type="submit" value="Get an answer" />
+        <form onSubmit={onSubmit} className={styles.form}>
+        <label className={styles.label}>
+        Ask a question
+        <input className={styles.input}
+          type="text"
+          name="topic"
+          placeholder="What is so special about this?"
+          value={topicInput}
+          onChange={(e) => setTopicInput(e.target.value)}
+        />
+      </label>
+      <label htmlFor="eli5">
+        <input
+  type="checkbox"
+  id="eli5"
+  name="eli5"
+  checked={isELI5Checked}
+  onChange={() => setIsELI5Checked(!isELI5Checked)}
+/>Explain it like I'm five</label>
+   
+      <input type="submit" value="Get an answer" />
         </form>
-        
+
       </main>
     </div>
   );
