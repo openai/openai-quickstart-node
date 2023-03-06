d7f78f758b8b2c35dde5afe54baeb7729ced98a0
Destross <felipesagredogalindo@gmail.com>
2023-03-06 18:50:00 -0300

    Agregando GoogleAdsense

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,8 @@ export default function Home() {
         <title>IA-FS</title>
         <link rel="icon" href="/marmota.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2718515830886252"
+        crossorigin="anonymous"></script>
       </Head>
       
       <NavigationBar/>
