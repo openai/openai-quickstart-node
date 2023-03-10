94ad35799acbe8f195f3e221e81cf820fbf15ec8
jaapstronks <jaapstronks@bureaubolster.nl>
2023-03-10 17:16:28 +0100

    updates

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,5 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai';
+import fs from 'fs';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -9,28 +10,32 @@ export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
-        message: "OpenAI API key not configured, please follow instructions in README.md",
-      }
+        message:
+          'OpenAI API key not configured, please follow instructions in README.md',
+      },
     });
     return;
   }
 
   const topic = req.body.topic || '';
-  const isELI5 = req.body.isELI5 || false; // Add this line to get the value of the isELI5 checkbox
+  const isELI5 = req.body.isELI5 || false;
+  const slug = req.body.slug || ''; // Get the slug from the request body
+console.log(slug);
 
   if (topic.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please ask a valid question.",
-      }
+        message: 'Please ask a valid question.',
+      },
     });
     return;
   }
 
+
   try {
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(topic, isELI5), // Pass the isELI5 value to the generatePrompt function
+      prompt: generatePrompt(topic, isELI5, slug), // Pass the isELI5 value to the generatePrompt function
       max_tokens: 800,
       temperature: 0.9,
 
@@ -53,23 +58,28 @@ export default async function (req, res) {
 }
 
 
-function generatePrompt(topic, isELI5) {
-  console.log(isELI5);
+
+
+function generatePrompt(topic, isELI5, slug) {
   const capitalizedTopic =
     topic[0].toUpperCase() + topic.slice(1).toLowerCase();
-  const prompt = `I want you to answer a question about an interior model made by Theo van Doesburg. In your answer, use whatever you know about this topic, but also take into account the following information. Also, don't try say too many things in every answer. The person asking the questions will ask followup questions, so tell one tidbit of information at a time, and expand a little about it. You can use the information provided in this prompt for that, but feel free to add existing knowledge to it. If you mention a certain style, place or person, use your general knowledge to explain what, where or who that is. Also, on a new line, make 1 suggestion for related followup questions the person can ask you, if they want to to dive deeper into a certain topic. Suggest these by saying "You could also ask me about..." or "Perhaps you would like to ask something about..." 
-  The interior model that is the subject of the question is a colour design for a ceiling and three walls, for a cinema and dance hall of Café de l'Aubette in Strasbourg, France. It is a dazzlingly incongruous expression of the 1920s De Stijl movement. Designed by Theo van Doesburg, one of the movement’s founders and leading lights, the Aubette’s minimalist, geometric aesthetic was heavily influenced by the work of contemporary artists such as Piet Mondrian. In designing the café’s interiors, Van Doesburg sought to do more than simply place viewers before a painting; he wanted to envelop them in it. The design, from 1926, is for Café Aubette, one of De Stijl's most important architectural projects. As the pioneer of Dutch De Stijl movement, Theo van Doesburg employed his perspective on elementarism and the neo-plastic style to decorate the ceiling and wall of cinema-ballroom with orthogonal composition in primary colors. 
-  It was made with pencil and gouache on cardboard, and it is 43 by 74.5 centimeters. It is the only model by Van Doesburg to survive after his death in 1931. At first sight, it seems to be a one-dimensional work. But the walls open outwards, as can be clearly seen in a photo from Van Doesburg’s studio. The model shows how Van Doesburg approached space as the synthesis of painting and architecture. The work is shown as it was acquired: framed. For decades, it was owned by the Swiss Galerie Gmurzynska and it was previously exhibited there as an autonomous visual work, framed on the wall. But such a strictly disciplinary approach was exactly what Van Doesburg was against: for him, a visual work could be an architectural project, model and autonomous artwork all at the same time. It is on display at het Nieuwe Instituut in Rotterdam, The Netherlands. In 1926, three avant-garde artists Theo van Doesburg, Sophie Taeuber-Arp and Jean Arp (or Hans Arp) were commissioned by Paul and Adré Horn to redecorate and design the Café Aubette in Strasbourg. Three artists were equally responsible for different sections of the building. Theo van Doesburg was in charge of the two cafés and two dance halls, Sophie Taeuber for the entrance aisle, tearoom, and two bars, and Jean Arp for the basement, the passage, and billiard room. And all three artists worked together designing the stairwell. The work of the three artists had been called "the Sistine Chapel of abstract art". This historical building still opens as a historical landmark nowadays.
 
+  let specificInfo = '';
+  try {
+    specificInfo = fs.readFileSync(`./data/${slug}.txt`, 'utf8');
+  } catch (error) {
+    console.error(`Error reading file: ${error.message}`);
+  }
+
+  const prompt = `I want you to answer a question about a heritage collection item. In your answer, use whatever you know about this topic, but also take into account the following information: ${specificInfo}. Also, don't try to say too many things in every answer. The person asking the questions will ask follow-up questions, so tell one tidbit of information at a time, and expand a little about it. You can use the information provided in this prompt for that, but feel free to add existing knowledge to it. If you mention a certain style, place or person, use your general knowledge to explain what, where or who that is. Also, on a new line, make 1 suggestion for related follow-up questions the person can ask you, if they want to dive deeper into a certain topic. Suggest these by saying "You could also ask me about..." or "Perhaps you would like to ask something about..."
+  
 Question: ${capitalizedTopic}
 Answer:`;
 
   if (isELI5) {
     console.log('eli5 was checked');
-    return `${prompt} Explain it to me like I'm a seven year old, so keep it simple, but don't dumb it down too much. If you use difficult words, such ash 'gouache' or 'elementarism', place them in quotes and explain what they mean.`;
-    
+    return `${prompt} Explain it to me like I'm a seven-year-old, so keep it simple, but don't dumb it down too much. If you use difficult words, such as 'gouache' or 'elementarism', place them in quotes and explain what they mean.`;
   }
 
   return prompt;
-  
 }
---
+++ pages/van-doesburg-interior-model.js
@@ -0,0 +1,147 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+import Typewriter from "typewriter-effect";
+
+
+export default function Home() {
+  const [topicInput, setTopicInput] = useState("");
+  const [result, setResult] = useState();
+  const [isELI5Checked, setIsELI5Checked] = useState(false);
+  const [isResultReady, setIsResultReady] = useState(false);
+
+
+  const questions = [
+    "What is interesting about this model?",
+    "How does this relate to 'The Style'?",
+    "What is the history behind this design?",
+    "How was this model made?",
+    "Who did Van Doesburg collaborate with?"
+  ];
+  
+  const randomIndex = Math.floor(Math.random() * 5);
+  const randomQuestion = questions[randomIndex];
+
+  async function onSubmit(event) {
+    event.preventDefault();
+  
+    // Get the slug from the URL
+    const slug = window.location.pathname.split('/')[1];
+  
+    try {
+      setResult(null); // Reset the result state when a new question is submitted
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ topic: topicInput, isELI5: isELI5Checked, slug: slug }), // Pass the slug as an additional argument
+  
+      });
+  
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`Request failed with status ${response.status}`);
+      }
+     
+      setResult(data.result);
+      setIsResultReady(true);
+      setTopicInput("");
+    } catch(error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+
+  
+
+
+  return (
+    <div>
+      <Head>
+        <title>Nieuwe Instituut CollectionBot</title>
+        <link rel="favicon" href="/favicon.png" />
+        <meta property="og:image" content="/opengraph.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:image" content="/opengraph.jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Nieuwe Instituut CollectionBot" />
+        <meta property="og:title" content="Nieuwe Instituut CollectionBot" />
+        <meta property="og:description" content="A prototype demonstrating the use of a generative AI application to disclose collection data." />
+        <meta property="twitter:description" content="A prototype demonstrating the use of a generative AI application to disclose collection data." />
+        
+      </Head>
+
+     
+        <header className={styles.header}>
+        <div className={styles.headercontainer}>
+        <img src="/ni-logo-small.png" className={styles.icon} />
+        </div>
+ 
+        </header>
+        <main className={styles.main}>
+
+        <h1 className={styles.title}><span className={styles.cerial}>Collecti</span>onBot</h1>
+        <p className={styles.intro}>This is a prototype application, testing the usage of the generative AI to disclose collection information.</p> 
+        
+        <p className={styles.regular}>This website was made using the <a href="https://openai.com/blog/introducing-chatgpt-and-whisper-apis">new ChatGPT API by OpenAI</a>. It prepends user questions with specific information about a collection item, allowing for a fair amount of control over the accuracy of the AI's response, while still benefiting from the AI's abilities to add all kinds of information in a customizable, conversational interface.</p>
+
+
+        <h3>Ask a question about Van Doesburg's interior model for l'Aubette</h3>
+        <img src="/maquette.jpeg" className={styles.image} alt="maquette Van Doesburg" />
+        <p className={styles.caption}>This is a model by architect and artist Theo van Doesburg.  Photo: Johannes Schwartz. </p>
+      
+        <p className={styles.regular}>Feel free to ask about more about the interior model for l'Aubette. For example, you could ask: what is this a model of? How was it made? What is special about it? Which movement is this an icon of? Where can it be seen in person? Ask away!</p>
+        <div className={styles.listwrapper}>
+        <ul className={styles.list}><li><strong>Tip 1</strong>: each answer takes a couple of seconds to generate, so be patient after clicking the 'Get answer' button.</li><li> <strong>Tip 2</strong>: if you don't know where to start, just ask 'What's so special about this?'</li> </ul>
+        </div>
+     
+        
+        {result && (
+  <div className={styles.result}>
+    <h3 className={styles.collectionboth3}>🤖 CollectionBot:</h3>
+    <Typewriter
+      onInit={(typewriter) => {
+        typewriter.typeString(result)
+          .callFunction(() => {
+            console.log('String typed out!');
+          })
+          .start();
+      }}
+      options={{
+        delay: 15
+      }}
+    />
+  </div>
+)}
+        <form id="my-form" onSubmit={onSubmit} className={styles.form}>
+        <label className={styles.label}>
+        Ask a question about this object
+        <input className={styles.input}
+          type="text"
+          name="topic"
+          placeholder={randomQuestion}
+          value={topicInput}
+          onChange={(e) => setTopicInput(e.target.value)}
+        />
+      </label>
+      <label htmlFor="eli5">
+        <input
+        className={styles.checkmark}
+  type="checkbox"
+  id="eli5"
+  name="eli5"
+  checked={isELI5Checked}
+  onChange={() => setIsELI5Checked(!isELI5Checked)}
+/><span className={styles.labeltext}>Explain it like I'm five</span></label>
+   
+      <input type="submit" value="Get an answer" />
+        </form>
+<p className={styles.disclaimer}>This prototype was made by Jaap Stronks.</p>
+      </main>
+    </div>
+  );
+}
