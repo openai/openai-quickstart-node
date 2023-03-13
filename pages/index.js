db4b96ac8e0100fec84dacb0a58f00fbfe3027fe
LEAP\Gabriel.McHugh <gabriel.t.mchugh@gmail.com>
2023-03-14 09:51:51 +1000

    setting up

--- pages/index.js
+++ pages/index.js
@@ -39,6 +39,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <div><h1>Gabriels Model</h1></div>
         <img src="/dog.png" className={styles.icon} />
         <h3>Name my pet</h3>
         <form onSubmit={onSubmit}>
