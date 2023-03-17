931c6fc4b47819a00d1211dc546372c0afda6232
jaapstronks <jaapstronks@bureaubolster.nl>
2023-03-17 16:30:54 +0100

    additions

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,7 @@
-const path = require('path');
-
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'nl'],
   },
-  localePath: path.resolve('./public/locales'),
-};
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,69 @@
 import Head from "next/head";
-import { useState } from "react";
 import styles from "./index.module.css";
 import Link from 'next/link'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 
 
 export default function Home() {
-
-
-
-  
-
+  const { t } = useTranslation('common');
 
   return (
     <div>
       <Head>
-        <title>Nieuwe Instituut CollectionBot</title>
+        <title>{t('title')}</title>
         <link rel="favicon" href="/favicon.png" />
         <meta property="og:image" content="/opengraph.jpg" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta name="twitter:image" content="/opengraph.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Nieuwe Instituut CollectionBot" />
-        <meta property="og:title" content="Nieuwe Instituut CollectionBot" />
-        <meta property="og:description" content="A prototype demonstrating the use of a generative AI application to disclose collection data." />
-        <meta property="twitter:description" content="A prototype demonstrating the use of a generative AI application to disclose collection data." />
-        
+        <meta name="twitter:title" content={t('title')} />
+        <meta property="og:title" content={t('title')} />
+        <meta property="og:description" content={t('description')} />
+        <meta property="twitter:description" content={t('description')} />
       </Head>
 
-     
-        <header className={styles.header}>
+      <header className={styles.header}>
         <div className={styles.headercontainer}>
-        <img src="/ni-logo-small.png" className={styles.icon} />
+          <img src="/ni-logo-small.png" className={styles.icon} />
         </div>
- 
-        </header>
-        <main className={styles.main}>
+      </header>
+      
+      <main className={styles.main}>
+        <h1 className={styles.title}>{t('title1')}<span className={styles.cerial}>{t('title2')}</span></h1>
+        <p className={styles.intro}>{t('intro')}</p> 
+        <p className={styles.regular}>{t('regular_1')}<a href="https://openai.com/blog/introducing-chatgpt-and-whisper-apis">{t('regular_2')}</a>{t('regular_3')}</p>
 
-        <h1 className={styles.title}><span className={styles.cerial}>Collecti</span>onBot</h1>
-        <p className={styles.intro}>This is a prototype application, testing the usage of the generative AI to disclose collection information.</p> 
-        
-        <p className={styles.regular}>This website was made using the <a href="https://openai.com/blog/introducing-chatgpt-and-whisper-apis">new ChatGPT API by OpenAI</a>. It prepends user questions with specific information about a collection item, allowing for a fair amount of control over the accuracy of the AI's response, while still benefiting from the AI's abilities to add all kinds of information in a customizable, conversational interface. The following collection items are available:</p>
+        <h4 className={styles.subtitle}>{t('frontpageSubtitle')}</h4>
+  
 
         <div className={styles.cardContainer}>
-  <Link href="/van-doesburg-interior-model" className={styles.card}>
-    
-      <img src="/maquette.jpeg" className={styles.cardImage} alt="Van Doesburg's interior model for l'Aubette" />
-      <div className={styles.titleWrapper}> <h4 className={styles.cardTitle}>Van Doesburg's interior model for l'Aubette</h4></div>
+          <Link href="/van-doesburg-interior-model" className={styles.card}>
+            <img src="/maquette.jpeg" className={styles.cardImage} alt={t('van_doesburg_alt')} />
+            <div className={styles.titleWrapper}><h4 className={styles.cardTitle}>{t('van_doesburg_title')}</h4></div>
+          </Link>
+          
+          <Link href="/sonneveld-house" className={styles.card}>
+            <img src="/sonneveld.jpg" className={styles.cardImage} alt={t('sonneveld_alt')} />
+            <div className={styles.titleWrapper}><h4 className={styles.cardTitle}>{t('sonneveld_title')}</h4></div>
+          </Link>
+        </div>
 
-  </Link>
-  <Link href="/sonneveld-house" className={styles.card}>
-  
-      <img src="/sonneveld.jpg" className={styles.cardImage} alt="Sonneveld House" />
-     <div className={styles.titleWrapper}><h4 className={styles.cardTitle}>Sonneveld House</h4></div>
-    
-  </Link>
-</div>
-
-
-        {/* <ul className={styles.list}><li className={styles.listitem}><Link href="/van-doesburg-interior-model">Van Doesburg's interior model for l'Aubette</Link></li>
-        <li className={styles.listitem}><Link href="/sonneveld-house">Sonneveld House</Link></li></ul> */}
-
-
-
-        
-     
-
-<p className={styles.disclaimer}>This prototype was made by Jaap Stronks.</p>
+        <p className={styles.disclaimer}>{t('disclaimer')}</p>
       </main>
     </div>
   );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        'common',
+        'footer',
+      ])),
+      // Will be passed to the page component as props
+    },
+  }
 }
