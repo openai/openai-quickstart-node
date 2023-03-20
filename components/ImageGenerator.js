374fa2ff7641323ab660ca9cbb4a6d64f4da0ea0
jaapstronks <jaapstronks@bureaubolster.nl>
2023-03-20 23:52:32 +0100

    fix

--- components/ImageGenerator.js
+++ components/ImageGenerator.js
@@ -46,18 +46,7 @@ const ImageGenerator = ({ prompt }) => {
     return <div>Error: {error}</div>;
   }
 
-  if (!image) {
-    return <div className={styles.status}><Typewriter
-    options={{
-      strings: ['Booting up the server', 'Hacking the mainframe', 'Painting pixels', 'Pondering the meaning of life'],
-      autoStart: true,
-      loop: true,
-      delay: 50,
-      skipAddStyles: true,
-      deleteSpeed: 30,
-    }}
-  /></div>;
-  }
+
 
   return <div className={styles.generated}><img className={styles.genimage} src={image} alt="Generated image" /></div>;
 };
