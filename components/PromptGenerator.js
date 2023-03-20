ce4c4286804e34e2f66419ee3279dbfd4e47ea72
jaapstronks <jaapstronks@bureaubolster.nl>
2023-03-20 23:04:44 +0100

    Upgrade

--- components/PromptGenerator.js
+++ components/PromptGenerator.js
@@ -4,8 +4,7 @@ import styles from "./PromptGenerator.module.css";
 const PromptGenerator = ({ onPromptSubmit }) => {
   const [item, setItem] = useState("");
   const [name, setName] = useState("");
-  const [prompt, setPrompt] = useState("");
-  const [explanation, setExplanation] = useState("");
+  const [explanation, setExplanation] = useState(null);
   const [explanationLoading, setExplanationLoading] = useState(false);
 
   const handleItemChange = (e) => {
@@ -16,54 +15,39 @@ const PromptGenerator = ({ onPromptSubmit }) => {
     setName(e.target.value);
   };
 
-  async function fetchImageCreationExplanation(prompt) {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!item || !name) {
+      alert("Please fill out both fields.");
+      return;
+    }
+    setExplanation(null)
+    setExplanationLoading(true);
     try {
-      const response = await fetch("/api/generate-explanation", {
+      const res = await fetch("/api/imageprompt-generation", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          topic: prompt,
-        }),
+        body: JSON.stringify({ topic: `${item} as designed by ${name}` }),
       });
 
-      const data = await response.json();
-      return data.result;
+      if (res.ok) {
+        const data = await res.json();
+        const generatedPrompt = data.result;
+        onPromptSubmit(generatedPrompt);
+        setExplanation(generatedPrompt);
+      } else {
+        const data = await res.json();
+        alert(data.error.message);
+      }
     } catch (error) {
-      console.error("Error fetching image creation explanation:", error);
-      return "Error fetching image creation explanation.";
+      alert("An error occurred during your request.");
+    } finally {
+      setExplanationLoading(false);
     }
-  }
-
-  const handleSubmit = async (e) => {
-    console.log("handleSubmit called");
-    e.preventDefault();
-    const newPrompt = `A realistic photograph showing how a brand new ${item} would look as if it was redesigned by the Dutch designer/architect ${name}, in the way that it still looks like a ${item} but has been radically transformed in the style of ${name}.`;
-    console.log("New prompt:", newPrompt);
-    setPrompt(newPrompt);
-    console.log("Prompt state:", prompt);
-
-    setExplanation(null);
-    setExplanationLoading(true);
-  
-    // Fetch image creation explanation from the API
-    const explanationPrompt = `You are an AI assistant trying to explain architectural styles to visitors of an architecture museum website. If you would create an image with the following prompt, asking you to create an image that shows the design of an item in the style of a certain architect, how would you create it? Can you explain how you would make sure that this architect's style would find its way into the image you would create? It is not necessary to point out that you yourself are not able to generate images. Assume that OpenAI has a different API that is called with this prompt and generates an image, but is unable to add an explanation. Please add a helpful explanation that explains how you think that the OpenAI image generator would probably operate. But assume that the web page already explains this, so it is best to cut to the chase and simply explain the process. An example response would begin like this: "In order to create an image for an ${item} ad designed by ${name}, an AI assistant will likely first study their previous works to understand the key features of their aesthetic...". But don't go overboard. When testing, your explanation of the prompt was often much more elaborate than the actual image that was generated. For example, earlier responses you gave said things like 'The AI may generate an image that places the redesigned headphones within an architectural setting or against a background that further highlights'. The actual images created by the API were not that elaborate at all. So assume that the image generator has only a moderate level of competence – but do not mention that in your reply. Always explain a little bit about the style of ${name}. The prompt is: ${newPrompt}`;
-       // Pass the generated prompt to the parent component
-       onPromptSubmit(newPrompt);
-
-
-
-    const fetchedExplanation = await fetchImageCreationExplanation(explanationPrompt);
-    console.log("Image creation explanation:", fetchedExplanation);
-  
-    // Set the explanation state
-    setExplanation(fetchedExplanation);
-    // Set explanation loading status to false
-    setExplanationLoading(false);
- 
   };
-  
+
 
   return (
     <div className={styles.prompt} >
@@ -91,13 +75,13 @@ const PromptGenerator = ({ onPromptSubmit }) => {
         <button className={styles.promptbutton} type="submit">Generate Image</button>
       </form>
 
-      {explanationLoading && <p>Generating explanation...</p>}
+      {explanationLoading && <p>Generating a good prompt first...</p>}
       
       {explanation && (
               
       <div className={styles.explanation}>
-        <h3>Image Creation Explanation</h3>
-        <p>ChatGPT's image generator doesn't offer explanations. We asked the text-only version of ChatGPT to explain how the image generator probably went to work with your request: </p>
+        <h3>The prompt we created</h3>
+        <p>ChatGPT created the following prompt for its image generator:</p>
         <p className={styles.response}>{explanation}</p>
       </div>
     )}
---
+++ pages/api/imageprompt-generation.js
@@ -0,0 +1,76 @@
+import { Configuration, OpenAIApi } from "openai";
+import axios from "axios";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const truncateResponse = (response, maxLength) => {
+  let sentences = response.split(/(?<=\.|\?|!)\s+/);
+  let truncated = "";
+  let index = 0;
+
+  while (index < sentences.length && (truncated.length + sentences[index].length) <= maxLength) {
+    truncated += sentences[index];
+    index++;
+  }
+
+  return truncated;
+};
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
+    });
+    return;
+  }
+
+  const topic = req.body.topic || "";
+
+  if (topic.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please ask a valid question.",
+      },
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-4",
+      messages: [
+        { role: "system", content: "You are a helpful assistant who will create a good prompt for an image generation service. You will be used on a museum website that gives insight into how a certain architect's style may be reflected in their work, and that also gives insight into how generative AI may be used to create interesting user experiences. You will be asked to create a prompt that lets an image generator, based on generative AI, create a realistic photographic image of an item as if it was designed by a certain Dutch architect. Reflect on the unique style of this architect's work, and be creative when thinking of ways this style could be recognized in the item if the architect in question would design it. This does not need to be realistic, it is more educational and informative to be extreme in the imagined application of the architect's style, because this is al part of an exercise in recognizing distinctive styles. Be specific and elaborate in the prompt you are generating for the image service and be sure to describe specific visual details. You could describe the type of camera, lighting and perspective and other specific characteristics of a photo. You could also elaborate on the purpose of the photo. It could be a photo by a newspaper journalist, or a commercial photo for a brochure. Remember that an image generator is only as good as the prompt it is given, so make sure the prompt is elaborate, detailed and specific. But importantly, don't make your prompt too long, it should be under 950 characters." },
+        { role: "user", content: topic },
+      ],
+      max_tokens: 1200,
+      temperature: 0.9,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+
+    let result = completion.data.choices[0].message.content.trim();
+    result = truncateResponse(result, 950);
+
+  res.status(200).json({ result });
+  } catch (error) {
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: "An error occurred during your request.",
+        },
+      });
+    }
+  }
+}
