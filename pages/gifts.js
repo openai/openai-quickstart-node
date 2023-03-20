2837f4deca4aac6e16b3c0d1f40f223f7d8cc867
Harry-Yates <hyates1@gmail.com>
2023-03-20 11:23:56 +0100

    changed flavicon

--- pages/gifts.js
+++ pages/gifts.js
@@ -36,7 +36,7 @@ export default function Home() {
     <div>
       <Head>
         <title>OpenAI Quickstart</title>
-        <link rel='icon' href='/dog.png' />
+        <link rel='icon' href='/present.png' />
       </Head>
 
       <main className={styles.main}>
