da329933aa041f4bd10a004e29546259b5871e14
jaapstronks <jaapstronks@bureaubolster.nl>
2023-03-20 14:41:37 +0100

    adds explanation

--- components/PromptGenerator.js
+++ components/PromptGenerator.js
@@ -5,6 +5,7 @@ const PromptGenerator = ({ onPromptSubmit }) => {
   const [item, setItem] = useState("");
   const [name, setName] = useState("");
   const [prompt, setPrompt] = useState("");
+  const [explanation, setExplanation] = useState("");
 
   const handleItemChange = (e) => {
     setItem(e.target.value);
@@ -14,17 +15,46 @@ const PromptGenerator = ({ onPromptSubmit }) => {
     setName(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  async function fetchImageCreationExplanation(prompt) {
+    try {
+      const response = await fetch("/api/generate-explanation", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          topic: prompt,
+        }),
+      });
+
+      const data = await response.json();
+      return data.result;
+    } catch (error) {
+      console.error("Error fetching image creation explanation:", error);
+      return "Error fetching image creation explanation.";
+    }
+  }
+
+  const handleSubmit = async (e) => {
     console.log("handleSubmit called");
     e.preventDefault();
     const newPrompt = `A realistic photograph showing how a brand new ${item} would look as if it was redesigned by the Dutch designer/architect ${name}, in the way that it still looks like a ${item} but has been radically transformed in the style of ${name}.`;
     console.log("New prompt:", newPrompt);
     setPrompt(newPrompt);
     console.log("Prompt state:", prompt);
-
+  
+    // Fetch image creation explanation from the API
+    const explanationPrompt = `You are an AI assistant trying to explain architectural styles to visitors of an architecture museum website. If you would create an image with the following prompt, asking you to create an image that shows the design of an item in the style of a certain architect, how would you create it? Can you explain how you would make sure that this architect's style would find its way into the image you would create? It is not necessary to point out that you yourself are not able to generate images. Assume that OpenAI has a different API that is called with this prompt and generates an image, but is unable to add an explanation. Please add a helpful explanation that explains how you think that the OpenAI image generator would probably operate. But assume that the web page already explains this, so it is best to cut to the chase and simply explain the process. An example response would begin like this: "In order to create an image for an ${item} ad designed by ${name}, an AI assistant will likely first study their previous works to understand the key features of their aesthetic...". The prompt is: ${newPrompt}`;
+    const fetchedExplanation = await fetchImageCreationExplanation(explanationPrompt);
+    console.log("Image creation explanation:", fetchedExplanation);
+  
+    // Set the explanation state
+    setExplanation(fetchedExplanation);
+  
     // Pass the generated prompt to the parent component
     onPromptSubmit(newPrompt);
   };
+  
 
   return (
     <div className={styles.prompt} >
@@ -51,6 +81,12 @@ const PromptGenerator = ({ onPromptSubmit }) => {
         
         <button className={styles.promptbutton} type="submit">Generate Image</button>
       </form>
+      {explanation && (
+      <div className={styles.explanation}>
+        <h3>Image Creation Explanation</h3>
+        <p>{explanation}</p>
+      </div>
+    )}
     </div>
   );
 };
