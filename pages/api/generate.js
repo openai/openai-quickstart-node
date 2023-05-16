bfc60f454d52e50bb1905408b39495faebeb5686
Robert Mads Christensen <61755754+madschristensen99@users.noreply.github.com>
2023-05-16 16:38:31 -0400

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -51,6 +51,12 @@ export default async function (req, res) {
 function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `you are an ai agent begrudingly willing to help with the following task: ${capitalizedAnimal}
-`;
+  return `Suggest three names for an animal that is a superhero.
+
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: ${capitalizedAnimal}
+Names:`;
 }
