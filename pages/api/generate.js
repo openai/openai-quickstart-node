44ea8ce2d14d3495b04dffe688e88a814ee732eb
Robert Mads Christensen <61755754+madschristensen99@users.noreply.github.com>
2023-05-16 17:34:04 -0400

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -26,16 +26,13 @@ export default async function (req, res) {
   }
 
   try {
-    const completion = await openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
-    ]
-);
-    res.status(200).json({ result: completion['choices'][0]['message']['content'] });
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      max_tokens: 2000,
+      prompt: generatePrompt(animal),
+      temperature: 0.6,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
