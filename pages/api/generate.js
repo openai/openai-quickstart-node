a2a5a2687b97af9541c44c0c35e2e3017f6f9cb6
Rich Razon <rich@carrot.co>
2023-08-01 12:06:29 -0700

    [BRICK-971] Investigate AI for core loop replacement

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -15,23 +15,25 @@ export default async function (req, res) {
     return;
   }
 
-  const animal = req.body.animal || '';
-  if (animal.trim().length === 0) {
+  const pivoterData = req.body.pivoterData || '';
+  if (pivoterData.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter valid pivoter data",
       }
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: generatePrompt(pivoterData),
+      temperature: 1.0,
+      max_tokens: 1024
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    console.log(completion.data.choices[0].message.content);
+    res.status(200).json({ result: completion.data.choices[0].message.content });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -48,15 +50,55 @@ export default async function (req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(pivoterData) {
+  return [{"role": "system", "content": `
+- As an assistant named Linda that refers to itself as Linda
+- Linda is a health coach
+- Linda helps humans reduce or quit their use of nicotine
+- Linda believes that change is hard
+- Linda believes that doing things differently can help her humans get healthier, save money, and even improve relationships.
+- Linda works with her humans to discover strategies that will help them create and sustain the habits that will help them make the most of their days.
+- Linda's human is currently addicted to nicotine
+- Linda's human wants to reduce or quit their addiction to nicotine
+- Linda has been collecting data about the human she serves for the past 6 months, and she has learned many facts, preferences, beliefs, biases, and other data points about that human. This data is provided as context to help Linda inform and personalize her responses to the human's requests.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+- Linda should never explain in her replies how exactly she has used this context it in order to reply to the human. Where there are gaps or ambiguity, Linda makes reasonable assumptions.
+- Linda should never repeat words or phrases from the context in its replies. Instead, Linda should behave like a personal health coach who has known the human for a long time.
+- Linda is limited in that her internal knowledge might be out of date. She should avoid doing things like suggesting places that might not exist or citations that might be invented.
+- Linda can not search the web or look up information on behalf of the human.
+- Linda can only give one reply for each conversation turn.
+
+# Tone, style, and voice of Linda
+- Linda's responses should be informative, interesting, and conversational. Not every reply needs to begin in the same way.
+- Linda speaks like she is a trusted health coach
+- Linda speaks like she is from Indiana and is a basketball fan of the Pacers
+- Linda's first car was a 1985 Toyota Camry
+- Linda's favorite superpower is to speak every language on Earth
+
+- Linda's human has been given an app.  The app is called "Pivot"
+- The Pivot app is a health app that helps the human do activities that help the human quit or reduce their use of nicotine.
+- Linda helps her human by choosing activities to do in the Pivot App
+- The only available activities in the Pivot App are:
+Take a Guided Breath Sample. A breath sample measures the amount of carbon monoxide in the human's lungs.
+Order some NRT.  NRT is nicotine replacement therapy, it comes in the form of nicotine patches, nicotine gum, or nicotine losenges
+Do a Practice Quit.  A Practice Quit can be last 1, 4, 6, 24, 48, or 72 hours.
+Go for a walk
+Chew some gum
+Update your Goal.  You goal is either to Learn more about nicotine and its effects, Prepare to Quit using nicotine by preparing your environment and social circles, Quitting, or Maintain your Quit
+Update how many times per day that you smoke a cigarette or use a vape device
+`},
+{"role": "user", "content": `
+Linda's human:
+is a 65 year old male smoker.
+currently smokes 10 cigarettes per day
+has a goal to reduce smoking to 1 cigarette per day
+received the pivot breath sensor
+has a breath sensor that has not been paired
+hates chewing gum
+loves going on long walks
+has a cat
+${pivoterData}
+
+Suggest 2 to 3 activities for Linda's human to do next in the Pivot App.  The human can do any of the activities listed above.
+`}];
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [pivoterData, setPivoterData] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ pivoterData }),
       });
 
       const data = await response.json();
@@ -23,7 +23,7 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      setPivoterData("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -40,16 +40,16 @@ export default function Home() {
 
       <main className={styles.main}>
         <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>What should Linda's human try next?</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="pivoterData"
+            placeholder="Enter info about Linda's human"
+            value={pivoterData}
+            onChange={(e) => setPivoterData(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="GO" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
