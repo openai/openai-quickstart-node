1c2462dbed6dd5c9aaf8d43cb09069a549c6596a
Graham Fujisawa <sfrrki@gmail.com>
2023-09-01 10:21:55 -0700

    add generateCity and prompt

---
+++ pages/api/generateCity.js
@@ -0,0 +1,70 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const city = req.body.city || '';
+  if (city.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid city",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(city),
+      temperature: 0.6,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(city) {
+  const capitalizedCity =
+    city[0].toUpperCase() + city.slice(1).toLowerCase();
+  return `I'm looking to make a new homepage for a city. I've got a number of categories that I'm hoping to fill for content for the webpage.
+  please generate the following content for the city of: ${capitalizedCity}
+
+  Three main stories
+`;
+
+//   return `Suggest three names for an animal that is a superhero.
+
+// Animal: Cat
+// Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+// Animal: Dog
+// Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+// Animal: ${capitalizedCity}
+// Names:`;
+
+
+}
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   async function onSubmit(event) {
     event.preventDefault();
     try {
-      const response = await fetch("/api/generate", {
+      const response = await fetch("/api/generateCity", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
