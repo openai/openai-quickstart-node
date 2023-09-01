882cf629881349447f001a86344c07cde2c41baf
Graham Fujisawa <sfrrki@gmail.com>
2023-09-01 10:40:19 -0700

    update animal nomenclature to city

--- pages/api/generateCity.js
+++ pages/api/generateCity.js
@@ -54,7 +54,50 @@ function generatePrompt(city) {
   return `I'm looking to make a new homepage for a city. I've got a number of categories that I'm hoping to fill for content for the webpage.
   please generate the following content for the city of: ${capitalizedCity}
 
-  Three main stories
+  Since this is going to be placed into a webpage, I'm hoping you can place the result in an array of seven elements. This way, the elements can be accessed individually.
+
+  - three new stories
+  - one story you can read more about
+  - three main stories to appear on tiles
+
+  Here is an example for Boston:
+
+  const bostonHomepageContent = [
+    // Three New Stories
+    {
+      title: "Exciting Cultural Festival in Boston",
+      description: "Experience the vibrant culture of Boston with the upcoming cultural festival, featuring music, art, and delicious cuisine.",
+    },
+    {
+      title: "New Public Transportation System Unveiled",
+      description: "Boston introduces an efficient and eco-friendly public transportation system, making commuting easier for all residents.",
+    },
+    {
+      title: "Tech Hub Expansion Spurs Job Growth",
+      description: "Boston's thriving tech industry continues to expand, offering exciting job opportunities in cutting-edge fields.",
+    },
+    
+    // One Story You Can Read More About
+    {
+      title: "Historic Freedom Trail Reopens",
+      description: "Discover the rich history of Boston as the iconic Freedom Trail reopens after extensive renovations. Learn more about the city's revolutionary past.",
+      link: "Read More",
+    },
+    
+    // Three Main Stories to Appear on Tiles
+    {
+      title: "Boston Harbor Revitalization Project",
+      description: "Explore the plans for the Boston Harbor revitalization project, creating a stunning waterfront destination for locals and tourists alike.",
+    },
+    {
+      title: "Boston's Culinary Delights",
+      description: "Indulge in Boston's diverse culinary scene, from seafood delights to international cuisine. Taste the flavors of the city.",
+    },
+    {
+      title: "Boston's Top Family-Friendly Attractions",
+      description: "Plan your family adventure in Boston with our guide to the city's top family-friendly attractions and activities.",
+    },
+  ];
 `;
 
 //   return `Suggest three names for an animal that is a superhero.
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [cityInput, setCityInput] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
@@ -14,7 +14,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ city: cityInput }),
       });
 
       const data = await response.json();
@@ -23,7 +23,7 @@ export default function Home() {
       }
 
       setResult(data.result);
-      setAnimalInput("");
+      setCityInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -44,12 +44,12 @@ export default function Home() {
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="city"
+            placeholder="Enter a city"
+            value={cityInput}
+            onChange={(e) => setCityInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate city data" />
         </form>
         <div className={styles.result}>{result}</div>
       </main>
